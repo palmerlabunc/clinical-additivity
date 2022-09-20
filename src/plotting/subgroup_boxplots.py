05d53451c2186bea513eedbdee2a561eafd514e8
Haeun Hwangbo <haeun0512@gmail.com>
2022-09-20 14:47:01 -0400

    changed aesthetics of boxplot

--- src/plotting/subgroup_boxplots.py
+++ src/plotting/subgroup_boxplots.py
@@ -20,8 +20,8 @@ def plot_box(df, group, order, model='add'):
     Returns:
         (plt.figure, plt.axes): tuple of figure and its associated axes
     """    
-    fig, ax = plt.subplots(figsize=(2, 1))
-
+    fig, ax = plt.subplots(figsize=(1.5, 1))
+    sns.despine()
     if model == 'ind':
         hr = 'HR_ind'
         xlab = 'HRHSA'
@@ -35,12 +35,12 @@ def plot_box(df, group, order, model='add'):
                   color='k', size=3, alpha=0.7, ax=ax)
     ax.axvline(1, linewidth=0.5, color='gray', alpha=0.5)
     ax.set_xscale('log', base=2)
-    x_major = [0.25, 0.5, 1, 2]
+    x_major = [0.5, 1, 2]
     ax.xaxis.set_major_locator(plticker.FixedLocator(x_major))
     ax.xaxis.set_major_formatter(plticker.FixedFormatter(x_major))
     ax.xaxis.set_minor_locator(plticker.MultipleLocator(base=0.1))
     ax.xaxis.set_minor_formatter(plticker.NullFormatter())
-    ax.set_xlim(0.25, 2)
+    ax.set_xlim(0.4, 2)
     ax.set_title(group)
     ax.set_ylabel("")
     ax.set_xlabel(xlab)
@@ -59,9 +59,9 @@ def plot_box_wrapper(feature):
     df.loc[:, feature] = df[feature].replace({0: 'No', 1: 'Yes'})
     _, p_yes = wilcoxon(np.log(df[df[feature] == 'Yes']['HR_add']))
     _, p_no = wilcoxon(np.log(df[df[feature] == 'No']['HR_add']))
-    fig, ax = plot_box(df, feature, ['Yes', 'No'], model='ind')
-    ax.text(0.28, 0, 'p={:.3f}'.format(p_yes), size=9)
-    ax.text(0.28, 1, 'p={:.3f}'.format(p_no), size=9)
+    fig, ax = plot_box(df, feature, ['Yes', 'No'], model='add')
+    ax.text(2, 0, 'p={:.3f}'.format(p_yes), size=9)
+    ax.text(2, 1, 'p={:.3f}'.format(p_no), size=9)
     return fig
 
 def plot_ici_boxplot():
@@ -101,12 +101,16 @@ def plot_HR_boxplot():
         plt.figure: plotted figure
     """
     _, df = import_input_data()
-    df.loc[:, 'HR binary'] = 'HR <= 0.6'
-    df.loc[df['HR(combo/control)'] > 0.6, 'HR binary'] = 'HR > 0.6'
-    _, p_high = wilcoxon(np.log(df[df['HR binary'] == 'HR > 0.6']['HR_add']))
-    _, p_low = wilcoxon(np.log(df[df['HR binary'] == 'HR <= 0.6']['HR_add']))
-    fig, ax = plot_box(df, 'HR binary', ['HR <= 0.6', 'HR > 0.6'], model='add')
-    ax.text(0.28, 0, 'p={:.3f}'.format(p_low), size=9)
-    ax.text(0.28, 1, 'p={:.3f}'.format(p_high), size=9)
-    ax.set_title('HR(combination vs. control)')
+    med = df['HR(combo/control)'].median()
+    df.loc[:, 'HR Lower Median'] = 'Yes'
+    df.loc[df['HR(combo/control)'] > med, 'HR Lower Median'] = 'No'
+    print("median", med)
+    _, p_low = wilcoxon(
+        np.log(df[df['HR Lower Median'] == 'Yes']['HR_add']))
+    _, p_high = wilcoxon(np.log(df[df['HR Lower Median'] == 'No']['HR_add']))
+
+    fig, ax = plot_box(df, 'HR Lower Median', ['Yes', 'No'], model='add')
+    ax.text(2, 0, 'p={:.3f}'.format(p_low), size=9)
+    ax.text(2, 1, 'p={:.3f}'.format(p_high), size=9)
+    ax.set_title('Net benefit of combo')
     return fig
